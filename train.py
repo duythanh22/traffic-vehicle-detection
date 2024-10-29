2757a48abee85c5bb41a90e006455c82f8d7c7d1
DuyThanhTonion <doduythanh22102002@gmail.com>
2024-10-29 20:34:55 +0700

    updates

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ python train.py --model fasterrcnn_resnet50_fpn --epochs 2 --use-train-aug --dat
 export CUDA_VISIBLE_DEVICES=0,1
 python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py --data data_configs/smoke.yaml --epochs 100 --model fasterrcnn_resnet50_fpn --name smoke_training --batch 16
 
-
+Duy Thanh
 """
 from torch_utils.engine import (
     train_one_epoch, evaluate, utils
